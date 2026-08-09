7223ac2deba3ed2ae1e00fb5981ce3c707373231
Natasha Ninan <nninan@amazon.com>
2026-08-08 20:02:38 -0700

    tweak: slow SPI bubble play speed to 550ms per tick

--- dashboard.py
+++ dashboard.py
@@ -1672,7 +1672,7 @@ def build_social_progress_tab():
                                value='ALL', clearable=False,
                                style={'minWidth': '230px'})],
                  className='control-group'),
-        timeline('sp', speed=350),
+        timeline('sp', speed=550),
     ])
     sp_section = html.Div([
         _sp_card('Global View', 'sp-bubble-title',
