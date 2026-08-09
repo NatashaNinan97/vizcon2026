5db6df8d3318a3de6699888d4f9cea4c43afbdd2
Natasha Ninan <nninan@amazon.com>
2026-08-08 20:05:53 -0700

    tweak: match SPI bubble play speed to pie chart (800ms)

--- dashboard.py
+++ dashboard.py
@@ -1672,7 +1672,7 @@ def build_social_progress_tab():
                                value='ALL', clearable=False,
                                style={'minWidth': '230px'})],
                  className='control-group'),
-        timeline('sp', speed=700),
+        timeline('sp'),
     ])
     sp_section = html.Div([
         _sp_card('Global View', 'sp-bubble-title',
